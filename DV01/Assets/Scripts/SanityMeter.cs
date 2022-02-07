b27d6709aaa90012299a4fbd5da89db8054aca1e
David489 <david05pav@gmail.com>
2022-02-07 21:27:34 +0100

    New Sanity Script

--- DV01/Assets/Scripts/SanityMeter.cs
+++ DV01/Assets/Scripts/SanityMeter.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class SanityMeter : MonoBehaviour
 {
+
+    public int sanityMeter = 70;
+    public GameObject dangerZone;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dangerZone.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void DangerousArea()
+    {
+        // Sanity goes down
+        dangerZone.SetActive(true);
+        while(sanityMeter != 0)
+        {
+            sanityMeter -= 1;
+        }
+        gameOver();
+    }
+
+    public void gameOver()
+    {
+
+    }
+
+    public void PositiveArea()
+    {
+        // Sanity goes up
+        while(sanityMeter != 100)
+        {
+            sanityMeter += 1;
+        }
+    }
+
+    public void sanityNeutral()
+    {
+        while(sanityMeter > 70)
+        {
+            sanityMeter -= 1;
+        }
     }
 }
