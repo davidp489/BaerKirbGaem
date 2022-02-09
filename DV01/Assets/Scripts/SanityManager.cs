4ef5ebac08323f137c1fba55b429f922279ac263
David489 <david05pav@gmail.com>
2022-02-09 21:37:20 +0100

    Sanity Manager prototype
    It works so far, what needs to be added is a delay after every sanity point is being lost

---
+++ DV01/Assets/Scripts/SanityManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SanityManager : MonoBehaviour
+{
+    public int sanityMeter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sanityMeter = 75;
+    }
+
+    
+
+    private void OnTriggerExit2D(Collider2D collider) // When entering dark/danger zone, sanity meter goes down
+    {
+        while(sanityMeter != 0)
+        {
+            StartCoroutine(sanityCoroutine());
+            sanityMeter--;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) // When entering light/safe zone, sanity meter goes up
+    {
+        if(collider.gameObject.CompareTag("light")) //Checks if the player entered the light area
+        {
+            while(sanityMeter != 100)
+            {
+                StartCoroutine(sanityCoroutine());
+                sanityMeter++;
+            }
+        }
+    }
+
+    IEnumerator sanityCoroutine()
+    {
+        yield return new WaitForSeconds(3);
+    }
+}
